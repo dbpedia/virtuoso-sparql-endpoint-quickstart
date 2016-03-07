60fffea96a9edd4ce26a27cc9cd7db071d0f1a77
gone-phishing <riteshoneinamillion@gmail.com>
2016-03-07 14:40:30 +0100

    Fixed rdf format filter and other minor fixes

--- download.sh
+++ download.sh
@@ -4,30 +4,29 @@ ${DLD_DEV:=}
 [[ ! -z "$DLD_DEV" ]] && set -x
 
 # Set default variables
-GENERIC_FILENAME=2015-04_dataid_lang.ttl
-BASEURL=http://downloads.dbpedia.org/2015-04/core-i18n/lang/2015-04_dataid_lang.ttl
+GENERIC_FILENAME="2015-04_dataid_lang.ttl"
+BASEURL="http://downloads.dbpedia.org/2015-04/core-i18n/lang/2015-04_dataid_lang.ttl"
 LANG="null"
 DIRECTORY="downloads"
 
-# Check if import directory exist or else make one
+# Check if downloads directory exist or else make one
 if [ ! -d "$DIRECTORY" ]; then
     # Control will enter here if $DIRECTORY doesn't exist.
     mkdir "$DIRECTORY"
     if [[ $? != 0 ]]; then
-    echo "Failed to make import directory. Delete any file named $DIRECTORY for the script to work."
+    echo "Failed to make $DIRECTORY directory. Delete any file named $DIRECTORY for the script to work."
     exit
   fi
 fi
+
 # Help function to display usage iunstructions
 function help 
 {
-  echo "Usage: $./upload.sh [options]
+  echo "Usage: $./download.sh [options]
   -l or --language : Set the language for which data-id file is to be downloaded [Required]
   -b or --baseurl  : Set the baseurl for fetching the data-id file [def: http://downloads.dbpedia.org/2015-04/core-i18n/lang/2015-04_dataid_lang.ttl]
   -t or --rdftype  : Set rdf format to download for datasets, [def: .ttl]
-  -h or --help     : Display this help text
-
-  -->Make sure docker daemon is running before launching the script"
+  -h or --help     : Display this help text"
 }
 
 # Setting all key value pairs specified as arguments to the script
@@ -64,7 +63,7 @@ done
 
 # Language parameter is mandatory. Check if specified or exit the script
 if [ "$LANG" == "null" ]; then
-  echo "Usage: $./upload.sh --help"
+  echo "Usage: $./download.sh --help"
   exit
 fi
 
@@ -77,8 +76,11 @@ echo $BASEURL | sed "s|lang|$LANG|g" | xargs wget
 # Get the filename of the above downloaded data-id file
 FILENAME=$( echo $GENERIC_FILENAME | sed "s|lang|$LANG|" )
 
+# Set the rdf filter
+RDFFILTER="$RDFTYPE.bz2"
+
 # Parse the file to get all download urls and store them in the downloadURLs.txt file
-cat $FILENAME | grep 'dcat:downloadURL' | grep "ttl.bz2" | sed -r -e 's|\s+dcat:downloadURL\s+<||' -e 's|> ;||' > downloadURLs.txt
+cat $FILENAME | grep 'dcat:downloadURL' | grep "$RDFFILTER" | sed -r -e 's|\s+dcat:downloadURL\s+<||' -e 's|> ;||' > downloadURLs.txt
 
 # Get the count of number of files to be downloaded
 COUNT=$( wc -l downloadURLs.txt | cut -d' ' -f1 )
@@ -87,7 +89,7 @@ echo "Number of files to be downloaded: " $COUNT
 # Set number of files downloaded to 0 to keep track of downloaded files
 DOWNLAODED=0
 
-# Download all the datasets and store them in import directory
+# Download all the datasets and store them in download directory
 for i in `cat downloadURLs.txt`; 
 do
   echo "Download URL: " $i
