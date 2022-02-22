96caa6fb9f4812b9ae841eb8e5d0a40700a84e0f
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-02-22 17:09:52 +0100

    Update import.sh

--- dbpedia-loader/import.sh
+++ dbpedia-loader/import.sh
@@ -74,6 +74,8 @@ run_virtuoso_cmd "registry_set ('dbp_category', '${DBP_CATEGORY}');"
 
 echo "[INFO] Installing VAD package 'dbpedia_dav.vad'"
 run_virtuoso_cmd "vad_install('/opt/virtuoso-opensource/vad/dbpedia_dav.vad', 0);"
+echo "[INFO] Installing VAD package 'fct_dav.vad'"
+run_virtuoso_cmd "vad_install('/opt/virtuoso-opensource/vad/fct_dav.vad', 0);"
 
 
 
