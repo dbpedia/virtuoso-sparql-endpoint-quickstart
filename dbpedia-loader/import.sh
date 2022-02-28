6f8bd492e4e1a77138951dfc503fad0121381501
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-02-28 17:26:38 +0100

    Update import.sh

--- dbpedia-loader/import.sh
+++ dbpedia-loader/import.sh
@@ -68,7 +68,9 @@ run_virtuoso_cmd "registry_set ('dbp_decode_iri', 'on');"
 
 echo "[INFO] IMPORT LAST META DATA DESC"
 cp -rf ./dbpedia_fr-metadata.ttl ${STORE_DATA_DIR}
+cp -rf ./dbpedia_fr-metadata.ttl ${DATA_DIR}
 cat ${STORE_DATA_DIR}/dbpedia_fr-metadata.ttl
+cat ${DATA_DIR}/dbpedia_fr-metadata.ttl
 run_virtuoso_cmd "ld_dir ('${STORE_DATA_DIR}', 'dbpedia_fr-metadata.ttl', '${DOMAIN}/graph/metadata');"
 
 
